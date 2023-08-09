df2101341e5e327a96677a9b9e5ae76a16a303a2
medhosnijendoubi <mohamedhosnijendoubi@gmail.com>
2023-08-09 15:45:33 +0000

    add all tasks files

---
+++ 0x00-python-hello_world/lists.h
@@ -0,0 +1,26 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdlib.h>
+
+/**
+ * struct listint_s - is a singly linked list
+ * @m: is an integer
+ * @next: is a points to the next node
+ *
+ * Description: is a singly linked list node structure
+ *
+ */
+typedef struct listint_s
+{
+	int m;
+	struct listint_s *next;
+} listint_t;
+
+size_t print_listint(const listint_t *h);
+listint_t *add_nodeint(listint_t **head, const int m);
+void free_listint(listint_t *head);
+int check_cycle(listint_t *list);
+
+#endif /* LISTS_H */
+
