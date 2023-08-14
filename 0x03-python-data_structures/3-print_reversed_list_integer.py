ce66d7350da3eb2c21c26719e499d460bb2692ad
medhosnijendoubi <mohamedhosnijendoubi@gmail.com>
2023-08-14 15:27:41 +0000

    add task 0,  1 ,2 and 3

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+#AUTHOR - Mohamed Hosni Jendoubi
+def print_reversed_list_integer(my_list=[]):
+    if isinstance(my_list, list):
+        my_list.reverse()
+        for i in my_list:
+            print("{:d}".format(i))
